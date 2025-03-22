170bb4dd0e5e78f07ed42cab56fb76b52661a20d
Bobby <thebobbyoneill227@gmail.com>
2025-03-22 00:53:30 -0400

    player triangle moves using wasd

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
-
 from constants import *
+from player import Player
+
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
-    pygame.init
+    pygame.init()
     
     print("Starting Asteroids!")
 
@@ -13,14 +16,25 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    BLACK = (0,0,0)
+    updatable = pygame.sprite.Group()
+
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(BLACK)
-        pygame.display.flip
+        delta_ms = clock.tick(60)
+        dt = delta_ms / 1000
+        screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
